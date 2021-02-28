7247f4626d517c2a6765f73cedf15b61e135e28e
pxa4084 <pxa4084@g.rit.edu>
2021-02-28 15:42:18 -0500

    added ability for judges to see portfolio periods

--- frontend/src/Judge/Page.js
+++ frontend/src/Judge/Page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Dashboard from './pages/Dashboard'
+import ScholarshipsDashboard from './pages/Scholarships'
 import Review from './pages/Review'
 import Vote from './pages/Vote'
 
@@ -12,6 +13,7 @@ const Judge = () => (
   <Layout>
     <Switch>
       <Route exact path='/' component={Dashboard} />
+      <Route exact path='/scholarships' component={ScholarshipsDashboard} />
       <Route exact path='/show/:id' component={Review} />
       <Route exact path='/show/:id/vote' component={Vote} />
       <Route component={NotFound} />
